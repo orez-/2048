930cc8b9e460d036cacb2c2d9db50eec863d278b
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2014-03-14 18:47:24 -0400

    TetrisPlanner bot, which won once\!

--- bots.py
+++ bots.py
@@ -6,6 +6,9 @@ LEFT, DOWN, RIGHT, UP = range(4)
 class Human(object):
     """
     Isn't a bot at all????????
+
+    Best score: ????
+    Worst score: ????
     """
     def get_action(self, board):
         inp = raw_input("> ")
@@ -19,6 +22,9 @@ class Human(object):
 class Tumbler(object):
     """
     Spins around. Isn't very good.
+
+    Best score: 256 (7)
+    Worst score: 64 (5)
     """
     def __init__(self):
         self.current = 0
@@ -33,6 +39,9 @@ class Insistent(object):
     """
     Insistent bot will keep trying one way. Once that way isn't valid,
     tries a new way.
+
+    Best score: 64 (5)
+    Worst score: 32 (4)
     """
     def __init__(self):
         self.direction = 0
@@ -43,16 +52,36 @@ class Insistent(object):
         return self.direction
 
 
+class Pendulum(object):
+    """
+    Pendulum swings back and forth between RIGHT and LEFT. Never goes UP.
+
+    Best score: 256 (7)
+    Worst score: Infinite loop
+    """
+    def __init__(self):
+        self.direction = 0
+
+    def get_action(self, board):
+        if self.direction == 3:
+            self.direction = 0
+            return 1
+        self.direction += 1
+        return self.direction - 1
+
+
 class Tetris(object):
     """
     Tetris bot never presses up.
+
+    Best score: 1024 (9)
+    Worst score: 256 (7)
     """
     def __init__(self):
         pass
 
     def get_action(self, board):
-        # return self.quick_look(board)
-        return self.bfs(board, 1000)
+        return self.one_step_average(board)
 
     def quick_look(self, board):
         best = (UP, 1)
@@ -64,41 +93,40 @@ class Tetris(object):
                     best = (dr, score)
         return best[0]
 
-
-    def bfs(self, board, depth):
-        best = (UP, 0)
-        actions = []
+    def one_step_average(self, board):
+        best = (UP, 1)
         for dr in (DOWN, LEFT, RIGHT):
             result = board.try_move(dr)
             if result is not False:
-                score = self.score(result)
-                actions.append((dr, score, result))
+                score = 0
+                two, four = self.possible_configs(result)
+                over = len(two)
+                score += sum((self.score(b) * 0.9 for b in two))
+                score += sum((self.score(b) * 0.1 for b in four))
+                score /= over
                 if score > best[1]:
                     best = (dr, score)
-        if not actions:
-            return UP
+        return best[0]
 
-        for _ in xrange(depth):
-            actions.sort(key=lambda (_, s, __): -s)
-            if not actions:
-                return best[0]
-            dr, score, board_state = actions.pop(0)
-            if score > best[1]:
-                best = (dr, score)
-            for _ in (DOWN, LEFT, RIGHT):
-                result = board_state.try_move(dr)
-                if result is not False:
-                    actions.append((dr, self.score(result), result))
-        return actions.pop(0)[0]
+    def possible_configs(self, board):
+        twos = []
+        fours = []
+        for (x, y) in board.open_spots:
+            for lst, num in zip((twos, fours), (0, 1)):
+                b = type(board)(board)  # TODO: write a clone
+                b.board[y][x] = num
+                lst.append(b)
+        return twos, fours
 
     def score(self, board):
         score = 10000
         if board.lost:
-            return -float("inf")
+            return 0
         if board.won:
             return float("inf")
-        tmp_board = [[0 if elem is None else 2 ** elem
-            for elem in row] for row in board.board]
+        tmp_board = [
+            [0 if elem is None else 2 ** elem for elem in row]
+            for row in board.board]
         for row, (above, below) in enumerate(zip(tmp_board, tmp_board[1:])):
             if max(above) < max(below):  # favor bigger numbers below
                 score += (max(below) - max(above)) * (2 ** row)
@@ -112,3 +140,57 @@ class Tetris(object):
         # favor more blank spaces (make more matches!)
         score += sum(map(lambda row: row.count(0), tmp_board)) * 1000
         return score
+
+
+class TetrisPlanner(Tetris):
+    """
+    A smarter Tetris that thinks ahead.
+
+    Best score: Won!
+    Worst score: 512 (8)
+    """
+    def get_action(self, board):
+        answer = self.two_step_average(board)
+        return answer
+
+    def two_step_average(self, board):
+        lookup = {board: {}}
+        # arrange lookup so that it maps boards to maps of directions to possible boards
+        for dr in (DOWN, LEFT, RIGHT):
+            result = board.try_move(dr)
+            if result is not False:
+                confs = self.possible_configs(result)
+                confs = map(lambda q: (0.9, q), confs[0]) + map(lambda q: (0.1, q), confs[1])
+                lookup[board][dr] = confs
+
+        # do that again.
+        for old_confs in lookup[board].values():
+            for odds, bd in old_confs:
+                if bd in lookup:  # already know how this one plays out
+                    continue
+                lookup[bd] = {}
+                for dr in (DOWN, LEFT, RIGHT):
+                    result = bd.try_move(dr)
+                    if result is not False:
+                        confs = self.possible_configs(result)
+                        confs = (
+                            map(lambda q: (0.9, q), confs[0]) +
+                            map(lambda q: (0.1, q), confs[1]))
+                        lookup[bd][dr] = confs
+
+        # figure out the best direction to go
+        return self.rec_best(board, lookup)[0]
+
+    def rec_best(self, board, lookup, mult=1):
+        best_score = 1
+        direction = UP
+        if board not in lookup:  # just take the estimated score
+            return (-1, self.score(board) * mult)
+        for dr, confs in lookup[board].items():
+            score = sum(
+                (self.rec_best(new_board, lookup, mlt)[1]
+                    for mlt, new_board in confs)) * 2 / len(confs)
+            if score > best_score:
+                best_score = score
+                direction = dr
+        return (direction, best_score)
